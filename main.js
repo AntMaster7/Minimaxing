ca3f0e612b2214164c72be34d9635f200212b9c7
Samuel Egger <se@adaris.ch>
2020-04-25 23:41:15 +0200

    Remove unrelated code and add comments,

--- main.js
+++ main.js
@@ -1,78 +1,23 @@
-function getBestMove(board, maxDepth) {
-    [score, move] = minimax(board, board.currentPlayer(), maxDepth, 0);
-    return move;
-}
-
-function minimax(board, player, maxDepth, currentDepth) {
-    // check if we're done recursing
-    if (board.isGameOver() || currentDepth == maxDepth) {
-        return [board.evaluate(player), null]
-    }
-
-    // otherwise bubble up values from below
-    var bestScore;
-    if (board.currentPlayer() == player) {
-        bestScore = Number.NEGATIVE_INFINITY;
-    } else {
-        bestScore = Number.POSITIVE_INFINITY;
-    }
-
-    var bestMove;
-    for (const move of board.getMoves()) {
-        var newBoard = board.makeMove(move);
-
-        // recurse
-        [currentScore, currentMove] = minimax(newBoard, player, maxDepth, currentDepth + 1);
-
-        // update the best score
-        if (board.currentPlayer() == player) {
-            if (currentScore > bestScore) {
-                bestScore = currentScore;
-                bestMove = move;
-            }
-        } else {
-            if (currentScore < bestScore) {
-                bestScore = currentScore;
-                bestMove = move;
-            }
-        }
-    }
-
-    board._state.value = bestMove.value;
-    return [bestScore, bestMove];
-}
-
 window.onload = function () {
-    function keydownHandler(e) {
-        if (e.keyCode == 13 && waitingForEnter) {
-            console.log("enter");
-        }
-    }
-
-    if (document.addEventListener) {
-        document.addEventListener('keydown', keydownHandler, false);
-    }
-    else if (document.attachEvent) {
-        document.attachEvent('onkeydown', keydownHandler);
-    }
-
     var canvas = document.getElementById("canvas"),
         context = canvas.getContext("2d"),
-        width = canvas.width = window.innerWidth,
-        height = canvas.height = window.innerHeight;
+        width = canvas.width = window.innerWidth;
+    var treeCenter = width / 2;
 
-    var root = this.parse([[[0], [0], [0]], [[[[2],[2]]], [1]]]);
+    // Create tree data from a simple array.
+    var tree = this.parse([[[0], [0], [0]], [[[[2],[2]]], [1]]]);
+    
+    // Draw the tree.
+    tree.draw(context, treeCenter, 100, 30, 40);
 
-    var radius = 30;
-    var x = width / 2;
-    root.draw(context, x, 100, radius, 40);
-
+    // Redraw tree if window size changed.
     window.onresize = function () {
         canvas.height = window.innerHeight;
-        root.draw(context, x, 100, radius, 40);
+        tree.draw(context, treeCenter, 100, radius, 40);
     };
 }
 
+// Creates a TreeNode structure from the given array.
 function parse(tree) {
     var root = new TreeNode(0);
     for (let index = 0; index < tree.length; index++) {
